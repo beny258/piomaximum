756d93d7871b99eb1ea153d8f864db61bd75de49
beny258 <benes.dav@gmail.com>
2022-12-06 17:28:42 +0100

    Updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,18 @@
 <!-- Footer -->
-<footer class="w3-container w3-padding-64 w3-center w3-opacity">
-  <div class="w3-xxxlarge">
+<footer class="w3-container w3-padding-64 w3-center">
+  <div class="w3-xxxlarge w3-opacity">
     <a href="mailto:david@stezka.org?subject=Piomaximum" target="_blank"><i class="fa fa-envelope w3-hover-opacity"></i></a>
     <a href="https://www.facebook.com/piomaximum" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
     <a href="https://www.instagram.com/piomaximum/" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
- </div>
- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    <a href="https://www.youtube.com/@piomaximum" target="_blank"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+    <a href="https://github.com/beny258/piomaximum" target="_blank"><i class="fa fa-github w3-hover-opacity"></i></a>
+  </div>
+  <div class="w3-padding-32">
+    <a href="https://pionyr.cz/" target="_blank"><img src="../img/pionyr_horiz_rgb.gif" height="40px" /></a>
+  </div>
+  <div class="w3-opacity">
+    <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+  </div>
 </footer>
 
 </body>
