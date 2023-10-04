c0c2a17987144ceaad7921c0189688d2a48547ba
beny258 <benes.dav@gmail.com>
2023-10-04 19:46:01 +0200

    Added square logo for small screens

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <!-- Header -->
 <header class="w3-container w3-center" style="padding: 100px 0px 0px 0px;">
-  <img src="img/logo-safezone.png" alt="Piomaximum" />
+  <img class="w3-hide-small" src="img/logo-safezone.png" alt="Piomaximum" />
+  <img class="w3-hide-large w3-hide-medium" src="img/logo-alt-safezone.png" alt="Piomaximum" />
 </header>
