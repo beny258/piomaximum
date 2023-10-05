42fb64cc14d885454db983e8759015d43b39b69b
beny258 <benes.dav@gmail.com>
2023-10-05 15:33:49 +0200

    Added contact page

--- kontakt.php
+++ kontakt.php
@@ -7,16 +7,36 @@
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
+    
+    <h1>Kde nás najdete</h1>
+    
+    <div class="w3-half">
+      <iframe style="border:none" src="https://frame.mapy.cz/s/mucemeleju" width="400" height="280" frameborder="0"></iframe>
+    </div>
+
+    <div class="w3-half w3-large">
+      <h2>Adresa</h2>
+      <p>
+        Jihomoravská krajská organizace Pionýra <br />
+        Údolní 58a <br />
+        Brno 60200
+      </p>
+
+      <h2>Kontakt</h2>
+      <a href="mailto:david@stezka.org?subject=Piomaximum" class="font-no-decor" target="_blank"><i class="fa fa-envelope"></i> david@stezka.org</a>
+    </div>
 
   </div>
 </div>
 
+<?php /*
 <!-- Second Grid -->
 <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
   <div class="w3-content">
-
+    
   </div>
 </div>
+*/ ?>
 
 <?php include_once "./includes/lecturer_banner.php"; // banner na shánění lektorů ?>
 <?php include_once "./includes/footer.php";          // paticka stranky ?>
